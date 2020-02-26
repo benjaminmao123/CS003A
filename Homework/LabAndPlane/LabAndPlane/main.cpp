317acb64f60fe0cd80631446ef31b48a624a060b
benjaminmao123 <43651347+benjaminmao123@users.noreply.github.com>
2020-02-26 13:58:08 -0800

    commit

--- Homework/LabAndPlane/LabAndPlane/main.cpp
+++ Homework/LabAndPlane/LabAndPlane/main.cpp
@@ -6,8 +6,6 @@ int array_size(int *sizes);
 void print_array(int *a);
 bool index_is_valid(int *sizes, int row, int col);
 
-
-
 //To call: int** p = allocate_twod<int>(sizes_array)   -Thank you Travis!
 template <class T>
 T **allocate_twod(int *sizes);
@@ -32,12 +30,103 @@ bool search_twod(T **twod, int *sizes, const T &key, int &row, int &col);
 template<class T>
 T *search_twod(T **twod, int *sizes, const T &key);
 
+int **init_lab(int *stations);
+bool login(int **labs, int lab, int station, int id);
+bool logout(int **labs, int *sizes, int id);
+
 template <class T>
 ostream &print_twod(T **twod, int *sizes, ostream &outs = cout);
 
 int main()
 {
+	int lab_sizes[] = { 4, 3, 2, -1 };
+	int **labs = init_lab(lab_sizes);
 
+	print_twod(labs, lab_sizes);
 
 	return 0;
+}
+
+template<class T>
+T **allocate_twod(int *sizes)
+{
+	T **labs;
+
+	int row = 0;
+
+	for (int *i = sizes; *i != -1; ++i)
+	{
+		++row;
+	}
+
+	labs = new T *[row];
+	T **labsEnd = labs + row;
+
+	for (T **i = labs; i != labsEnd; ++i)
+	{
+		*i = new T[*sizes++];
+	}
+
+	return labs;
+}
+
+template<class T>
+void init_twod(T **twod, int *sizes, T init_item)
+{
+	T **twodWalker = twod;
+
+	for (int *i = sizes; *i != -1; ++i)
+	{
+		T *rowEnd = *twodWalker + *i;
+
+		for (T *k = *twodWalker; k != rowEnd; ++k)
+		{
+			*k = init_item;
+		}
+
+		++twodWalker;
+	}
+}
+
+template<class T>
+ostream &print_twod(T **twod, int *sizes, ostream &outs)
+{
+	T **twodWalker = twod;
+
+	for (int *i = sizes; *i != -1; ++i)
+	{
+		T *rowEnd = *twodWalker + *i;
+
+		for (T *k = *twodWalker; k != rowEnd; ++k)
+		{
+			outs << *k << " ";
+		}
+
+		outs << endl;
+
+		++twodWalker;
+	}
+
+	return outs;
+}
+
+int array_size(int *sizes)
+{
+	return *sizes;
+}
+
+void print_array(int *a)
+{
+	for (int *i = a; *i != -1; ++i)
+	{
+		cout << *i << " ";
+	}
+}
+
+int **init_lab(int *stations)
+{
+	int **labs = allocate_twod<int>(stations);
+	init_twod(labs, stations, 0);
+
+	return labs;
 }
