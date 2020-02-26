c326b5e979dbf03014ac37decae3d4e4b1d00b0b
benjaminmao123 <43651347+benjaminmao123@users.noreply.github.com>
2020-02-26 01:52:47 -0800

    Update z_work_report.h

--- Homework/MomCounter/MomCounter/z_work_report.h
+++ Homework/MomCounter/MomCounter/z_work_report.h
@@ -3,14 +3,14 @@
 /*
     Features:
         -Not Implemented: None
-        -Implemented: deleteRepeats is implemented
+        -Implemented: All MCounter methods are implemented
         -Partly implemented: None
 
     Bugs    : features that are implemented are bug-free.
 
     Reflections:
-        This was an interesting assignment. It gave me good
-        practice using walkers instead of array notation to
-        deal with pointers.
+        This was a good exercise to practice object-oriented
+        design. I also learned that it is good to return something
+        in a function to indicate its status.
 */
 #endif // Z_WORK_REPORT_H
