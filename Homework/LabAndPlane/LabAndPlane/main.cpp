cec6e6f4559ce2025d4a4ec426baf9ad010ff733
benjaminmao123 <43651347+benjaminmao123@users.noreply.github.com>
2020-02-26 18:27:31 -0800

    Update main.cpp

--- Homework/LabAndPlane/LabAndPlane/main.cpp
+++ Homework/LabAndPlane/LabAndPlane/main.cpp
@@ -44,12 +44,18 @@ int main()
 	int **labs = init_lab(lab_sizes);
 
 	char command = '\0';
+	int id, lab, station;
 
 	while (command != 'x')
 	{
+		cout << "Plane:" << endl;
+		print_twod(labs, lab_sizes);
+		cout << endl;
+
 		cout << "i. Login" << endl;
 		cout << "o. Logout" << endl;
 		cout << "x. Exit" << endl;
+
 		cout << "\nCommand: ";
 		cin >> command;
 		cout << endl;
@@ -64,13 +70,59 @@ int main()
 			cout << endl;
 
 			cout << "ID: ";
-			
+			cin >> id;
+
+			if (!id)
+			{
+				cout << "Error: ID cannot be 0" << endl;
+				break;
+			}
+
+			cout << "Lab: ";
+			cin >> lab;
+
+			cout << "Station: ";
+			cin >> station;
+			cout << endl;
+
+			if (index_is_valid(lab_sizes, lab, station))
+			{
+				if (login(labs, lab, station, id))
+				{
+					cout << "Success: You are logged in to lab " << lab 
+						 << " station " << station << endl;
+				}
+				else
+				{
+					cout << "Error: Lab " << lab << " station " << station 
+						 << " is occupied" << endl;
+				}
+			}
+			else
+			{
+				cout << "Error: Lab " << lab << " station " << station 
+					 << " is invalid" << endl;
+			}
 			break;
 		case 'o':
+			cout << "ID: ";
+			cin >> id;
+			cout << endl;
+
+			if (logout(labs, lab_sizes, id) && id)
+			{
+				cout << "Success: You have been logged out" << endl;
+			}
+			else
+			{
+				cout << "Error: ID does not exist or you are not logged in" << endl;
+			}
 			break;
 		default:
 			break;
 		}
+
+		cout << endl;
 	}
 
 	return 0;
@@ -83,13 +135,10 @@ T **allocate_twod(int *sizes)
 
 	int row = 0;
 
-	for (int *i = sizes; *i != -1; ++i)
-	{
-		++row;
-	}
+	int numRows = array_size(sizes);
 
-	labs = new T *[row];
-	T **labsEnd = labs + row;
+	labs = new T *[numRows];
+	T **labsEnd = labs + numRows;
 
 	for (T **i = labs; i != labsEnd; ++i)
 	{
@@ -97,6 +146,24 @@ T **allocate_twod(int *sizes)
 	}
 
 	return labs;
+}
+
+template<class T>
+T read_twod(T **twod, int row, int col)
+{
+	return *(*(twod + row) + col);
+}
+
+template<class T>
+void write_twod(T **twod, int row, int col, const T &item)
+{
+	*(*(twod + row) + col) = item;
+}
+
+template<class T>
+T &get_twod(T **twod, int row, int col)
+{
+	return *(*(twod + row) + col);
 }
 
 template<class T>
@@ -115,6 +182,44 @@ void init_twod(T **twod, int *sizes, T init_item)
 
 		++twodWalker;
 	}
+}
+
+template<class T>
+bool search_twod(T **twod, int *sizes, const T &key, int &row, int &col)
+{
+	T **twodWalker = twod;
+
+	int sizesLength = array_size(sizes);
+
+	for (row = 0; row < sizesLength; ++row, ++twodWalker)
+	{
+		int colLength = *sizes++;
+
+		for (col = 0; col < colLength; ++col)
+		{
+			T *colElement = *twodWalker + col;
+
+			if (*colElement == key)
+			{
+				return true;
+			}
+		}
+	}
+
+	return false;
+}
+
+template<class T>
+T *search_twod(T **twod, int *sizes, const T &key)
+{
+	int row, col;
+
+	if (search_twod(twod, sizes, key, row, col))
+	{
+		return (*(twod + row) + col);
+	}
+
+	return nullptr;
 }
 
 template<class T>
@@ -141,7 +246,14 @@ ostream &print_twod(T **twod, int *sizes, ostream &outs)
 
 int array_size(int *sizes)
 {
-	return *sizes;
+	int size = 0;
+
+	for (int *i = sizes; *i != -1; ++i)
+	{
+		++size;
+	}
+
+	return size;
 }
 
 void print_array(int *a)
@@ -150,6 +262,21 @@ void print_array(int *a)
 	{
 		cout << *i << " ";
 	}
+}
+
+bool index_is_valid(int *sizes, int row, int col)
+{
+	if (row < array_size(sizes))
+	{
+		int *sizeLocation = sizes + row;
+
+		if (col < *sizeLocation && col >= 0)
+		{
+			return true;
+		}
+	}
+
+	return false;
 }
 
 int **init_lab(int *stations)
@@ -162,7 +289,28 @@ int **init_lab(int *stations)
 
 bool login(int **labs, int lab, int station, int id)
 {
+	int &mStation = get_twod(labs, lab, station);
 
+	if (!mStation)
+	{
+		mStation = id;
+
+		return true;
+	}
+
+	return false;
+}
+
+bool logout(int **labs, int *sizes, int id)
+{
+	int *element = search_twod(labs, sizes, id);
+
+	if (element)
+	{
+		*element = 0;
+
+		return true;
+	}
 
 	return false;
 }
