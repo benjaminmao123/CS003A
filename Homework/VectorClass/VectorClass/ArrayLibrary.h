c037bc261db09d83ca9458a4dd8198bd129dfc46
benjaminmao123 <43651347+benjaminmao123@users.noreply.github.com>
2020-05-07 13:00:35 -0700

    update vector

--- Homework/VectorClass/VectorClass/ArrayLibrary.h
+++ Homework/VectorClass/VectorClass/ArrayLibrary.h
@@ -10,22 +10,22 @@
 #pragma once
 
 template <typename T>
-T *add_entry(T *list, const T &new_entry, unsigned int &size, unsigned int &capacity);
+T *add_entry(T *list, const T &new_entry, int &size, int &capacity);
 
 template <typename T>
-T *remove_entry(T *list, const T &delete_me, unsigned int &size, unsigned int &capacity);
+T *remove_entry(T *list, const T &delete_me, int &size, int &capacity);
 
 template <typename T>
-T *allocate(T *list, const unsigned int capacity);
+T *allocate(T *list, const int capacity);
 
 template <typename T>
-T *reallocate(T *list, const unsigned int size, const unsigned int capacity);
+T *reallocate(T *list, const int size, const int capacity);
 
 template <typename T>
-void copy_list(T *dest, const T *src, const unsigned int size);
+void copy_list(T *dest, const T *src, const int size);
 
 template <typename T>
-T *search_entry(T *list, const T &find_me, const unsigned int size, unsigned int &index);
+T *search_entry(T *list, const T &find_me, const int size, int &index);
 
 template <typename T>
 void shift_left(T *start, const T *end);
@@ -36,18 +36,21 @@ void shift_right(T *start, const T *end);
 template <typename T>
 void delete_array(T *&list);
 
+template <typename T>
+void remove_last(T *&list, int &size, int &capacity);
+
 /*
 	Appends an element to the given array.
 
 	@param <T *list>: Original array.
 	@param <T &new_entry>: Value to append.
-	@param <unsigned int &size>: Size of the array.
-	@param <unsigned int &capacity>: Capacity of the array.
+	@param <int &size>: Size of the array.
+	@param <int &capacity>: Capacity of the array.
 
 	@return <T*>: New array with appended element.
 */
 template<typename T>
-T *add_entry(T *list, const T &new_entry, unsigned int &size, unsigned int &capacity)
+T *add_entry(T *list, const T &new_entry, int &size, int &capacity)
 {
 	if (size >= capacity)
 	{
@@ -82,16 +85,16 @@ T *add_entry(T *list, const T &new_entry, unsigned int &size, unsigned int &capa
 
 	@param <T *list>: Original array.
 	@param <const T &delete_me>: Value to remove.
-	@param <unsigned int &size>: Size of the original array.
-	@param <unsigned int &capacity>: Capacity of the array.
+	@param <int &size>: Size of the original array.
+	@param <int &capacity>: Capacity of the array.
 
 	@return <T*>: A new array without the deleted element.
 */
 template<typename T>
-T *remove_entry(T *list, const T &delete_me, unsigned int &size, unsigned int &capacity)
+T *remove_entry(T *list, const T &delete_me, int &size, int &capacity)
 {
 	//find the element to delete
-	unsigned int index = 0;
+	int index = 0;
 	T *deleteEntry = search_entry(list, delete_me, size, index);
 	T *newList = list;
 
@@ -118,13 +121,13 @@ T *remove_entry(T *list, const T &delete_me, unsigned int &size, unsigned int &c
 	Allocate memory to a given array.
 
 	@param <T *list>: Array to allocate memory for.
-	@param <const unsigned int capacity>: Determines the amount of memory to
+	@param <const int capacity>: Determines the amount of memory to
 		allocate.
 
 	@return <T*>: New array with a given capacity.
 */
 template<typename T>
-T *allocate(T *list, const unsigned int capacity)
+T *allocate(T *list, const int capacity)
 {
 	list = new T[capacity]();
 
@@ -132,7 +135,7 @@ T *allocate(T *list, const unsigned int capacity)
 }
 
 template<typename T>
-T *reallocate(T *list, const unsigned int size, const unsigned int capacity)
+T *reallocate(T *list, const int size, const int capacity)
 {
 	//allocate memory for new list
 	T *newList = nullptr;
@@ -150,10 +153,10 @@ T *reallocate(T *list, const unsigned int size, const unsigned int capacity)
 
 	@param <T *dest>: The array to copy to.
 	@param <T *src>: The array to copy from.
-	@param <const unsigned int size>: Size of the source array.
+	@param <const int size>: Size of the source array.
 */
 template<typename T>
-void copy_list(T *dest, const T *src, const unsigned int size)
+void copy_list(T *dest, const T *src, const int size)
 {
 	T *destEnd = dest + size;
 
@@ -169,13 +172,13 @@ void copy_list(T *dest, const T *src, const unsigned int size)
 
 	@param <T *list>: The array to search.
 	@param <T &find_me>: The value to search for.
-	@param <const unsigned int size>: Size of the array.
+	@param <const int size>: Size of the array.
 
 	@return <T*>: If the element is found, return a pointer
 		to the first element found. Else, return nullptr.
 */
 template<typename T>
-T *search_entry(T *list, const T &find_me, const unsigned int size, int &index)
+T *search_entry(T *list, const T &find_me, const int size, int &index)
 {
 	T *listEnd = list + size;
 	index = 0;
@@ -199,7 +202,7 @@ T *search_entry(T *list, const T &find_me, const unsigned int size, int &index)
 	Shifts the array elements left starting from a specified index.
 
 	@param <T *start>: position to shift left from.
-	@param <const unsigned int size>: Size of the array.
+	@param <const int size>: Size of the array.
 */
 template<typename T>
 void shift_left(T *start, const T *end)
@@ -217,7 +220,7 @@ void shift_left(T *start, const T *end)
 	Shifts the array elements right starting from a specified index.
 
 	@param <T *start>: position to shift right from.
-	@param <const unsigned int size>: Size of the array.
+	@param <const int size>: Size of the array.
 */
 template<typename T>
 void shift_right(T *start, const T *end)
@@ -234,4 +237,20 @@ template<typename T>
 inline void delete_array(T *&list)
 {
 	delete[] list;
+}
+
+template<typename T>
+inline void remove_last(T *&list, int &size, int &capacity)
+{
+	T *newList = list;
+
+	//if the size drops below a certain amount decrease capacity
+	if (size <= capacity / 4)
+	{
+		capacity /= 2;
+	}
+
+	--size;
+
+	newList = reallocate(list, size, capacity);
 }
--- Homework/VectorClass/VectorClass/Vector.h
+++ Homework/VectorClass/VectorClass/Vector.h
@@ -20,34 +20,38 @@ template <typename T>
 class Vector
 {
 public:
-    Vector(const unsigned int size = 0);
+    Vector(const int size = 0);
     Vector(const Vector &other);
     ~Vector();
 
-    const T operator[](const unsigned int index) const;
-    T &operator[](const unsigned int index);
-    T &at(const unsigned int index);                        //return reference to item at position index
-    const T at(const unsigned int index) const;             //return a const item at position index
+    const T operator[](const int index) const;
+    T &operator[](const int index);
+    T &at(const int index);                        //return reference to item at position index
+    const T at(const int index) const;             //return a const item at position index
     T &front() const;                                       //return item at position 0.
+    T &front();
     T &back() const;                                        //return item at the last position
+    T &back();
 
     //Push and Pop functions:
     Vector &operator+=(const T &item);                      //push_back
     void push_back(const T &item);                          //append to the end
-    T pop_back();                                           //remove last item and return it
+    void pop_back();                                           //remove last item
 
     //Insert and Erase:
-    void insert(const unsigned int pos, const T &item);     //insert at pos
-    void erase(const unsigned int erase_index);             //erase item at position
+    void insert(const int pos, const T &item);     //insert at pos
+    void erase(const int erase_index);             //erase item at position
     int index_of(const T &item);                            //search for item. retur index.
 
     //size and capacity:
-    void set_size(const unsigned int size);                 //enlarge the vector to this size
-    void set_capacity(const unsigned int capacity);         //allocate this space
-    unsigned int size() const { return sz; }                //return sz 
-    unsigned int capacity() const { return cap; }           //return cap
+    void set_size(const int size);                 //enlarge the vector to this size
+    void set_capacity(const int capacity);         //allocate this space
+    int size() const { return sz; }                //return sz 
+    int capacity() const { return cap; }           //return cap
 
     bool empty() const;                                     //return true if vector is empty
+    void swap(Vector &v);
+    void clear();
 
     //OUTPUT:
     template <class U>
@@ -61,20 +65,18 @@ public:
     Vector &operator=(const Vector &rhs);
 
 private:
-    void swap(Vector &v);
-
-    unsigned int sz;
-    unsigned int cap;
+    int sz;
+    int cap;
     T *data;
 };
 
 /*
     @summary: Overloaded constructor that takes in a size.
 
-    @param <const unsigned int size>: Size to set vector to.
+    @param <const int size>: Size to set vector to.
 */
 template<typename T>
-inline Vector<T>::Vector(const unsigned int size)
+inline Vector<T>::Vector(const int size)
     : sz(0), cap(1), data(nullptr)
 {
     if (size)
@@ -113,12 +115,12 @@ inline Vector<T>::~Vector()
     @summary: Overloaded subscript operator that retrieves
     element at given location.
 
-    @param <const unsigned int index>: Index to retrieve element.
+    @param <const int index>: Index to retrieve element.
 
     @return <const T>: Returns a copy of the element at index.
 */
 template<typename T>
-inline const T Vector<T>::operator[](const unsigned int index) const
+inline const T Vector<T>::operator[](const int index) const
 {
     if (index >= sz)
     {
@@ -134,12 +136,12 @@ inline const T Vector<T>::operator[](const unsigned int index) const
     @summary: Overloaded subscript operator that retrieves
     element at given location.
 
-    @param <const unsigned int index>: Index to retrieve element.
+    @param <const int index>: Index to retrieve element.
 
     @return <T &>: Returns a reference to the element at index.
 */
 template<typename T>
-inline T &Vector<T>::operator[](const unsigned int index)
+inline T &Vector<T>::operator[](const int index)
 {
     if (index >= sz)
     {
@@ -155,12 +157,12 @@ inline T &Vector<T>::operator[](const unsigned int index)
     @summary: Retrieves the element at the given index. If invalid
     argument is given, assert.
 
-    @param <const unsigned int index>: Index to retrieve element.
+    @param <const int index>: Index to retrieve element.
 
     @return <T &>: Returns a reference to the element at index.
 */
 template<typename T>
-inline T &Vector<T>::at(const unsigned int index)
+inline T &Vector<T>::at(const int index)
 {
     if (index >= sz)
     {
@@ -176,12 +178,12 @@ inline T &Vector<T>::at(const unsigned int index)
     @summary: Retrieves the element at the given index. If invalid
     argument is given, assert.
 
-    @param <const unsigned int index>: Index to retrieve element.
+    @param <const int index>: Index to retrieve element.
 
     @return <const T>: Returns a copy to the element at index.
 */
 template<typename T>
-inline const T Vector<T>::at(const unsigned int index) const
+inline const T Vector<T>::at(const int index) const
 {
     if (index >= sz)
     {
@@ -204,6 +206,12 @@ inline T &Vector<T>::front() const
     return at(0);
 }
 
+template<typename T>
+inline T &Vector<T>::front()
+{
+    return at(0);
+}
+
 /*
     @summary: Retrieves the element the last element.
 
@@ -211,6 +219,12 @@ inline T &Vector<T>::front() const
 */
 template<typename T>
 inline T &Vector<T>::back() const
+{
+    return at(sz - 1);
+}
+
+template<typename T>
+inline T &Vector<T>::back()
 {
     return at(sz - 1);
 }
@@ -245,23 +259,20 @@ inline void Vector<T>::push_back(const T &item)
     @return <T>: Returns the popped item.
 */
 template<typename T>
-inline T Vector<T>::pop_back()
+inline void Vector<T>::pop_back()
 {
-    T &item = at(sz - 1);
-    data = remove_entry(data, at(sz - 1), sz, cap);
-
-    return item;
+    remove_last(data, sz, cap);
 }
 
 /*
     @summary: Inserts an item to the container. Resizes the container
     if sz >= cap.
 
-    @param <const unsigned int pos>: Position to insert to.
+    @param <const int pos>: Position to insert to.
     @param <const T &item>: Item to insert.
 */
 template<typename T>
-inline void Vector<T>::insert(const unsigned int pos, const T &item)
+inline void Vector<T>::insert(const int pos, const T &item)
 {
     if (pos >= sz)
     {
@@ -285,10 +296,10 @@ inline void Vector<T>::insert(const unsigned int pos, const T &item)
     @summary: Erases the item at given index. If index >= size of vector,
         pop_back is used.
 
-    @param <const unsigned int erase_index>: Index of item to erase.
+    @param <const int erase_index>: Index of item to erase.
 */
 template<typename T>
-inline void Vector<T>::erase(const unsigned int erase_index)
+inline void Vector<T>::erase(const int erase_index)
 {
     if (erase_index >= sz)
     {
@@ -321,10 +332,10 @@ inline int Vector<T>::index_of(const T &item)
 /*
     @summary: Sets the size of the container.
 
-    @param <const unsigned int size>: Value to set size to.
+    @param <const int size>: Value to set size to.
 */
 template<typename T>
-inline void Vector<T>::set_size(const unsigned int size)
+inline void Vector<T>::set_size(const int size)
 {
     while (size >= cap)
     {
@@ -337,10 +348,10 @@ inline void Vector<T>::set_size(const unsigned int size)
 /*
     @summary: Sets the capacity of the container.
 
-    @param <const unsigned int capacity>: Value to set capacity to.
+    @param <const int capacity>: Value to set capacity to.
 */
 template<typename T>
-inline void Vector<T>::set_capacity(const unsigned int capacity)
+inline void Vector<T>::set_capacity(const int capacity)
 {
     if (capacity > sz)
     {
@@ -380,7 +391,7 @@ inline bool Vector<T>::empty() const
 template<class U>
 inline ostream &operator<<(ostream &outs, const Vector<U> &_a)
 {
-    for (unsigned int i = 0; i < _a.sz; ++i)
+    for (int i = 0; i < _a.sz; ++i)
     {
         outs << _a.at(i) << " ";
     }
@@ -404,7 +415,7 @@ inline bool Vector<T>::operator==(const Vector<T> &_a)
         return false;
     }
 
-    for (unsigned int i = 0; i < sz; ++i)
+    for (int i = 0; i < sz; ++i)
     {
         if (at(i) != _a.at(i))
         {
@@ -431,7 +442,7 @@ inline bool Vector<T>::operator!=(const Vector<T> &_a)
         return true;
     }
 
-    for (unsigned int i = 0; i < sz; ++i)
+    for (int i = 0; i < sz; ++i)
     {
         if (at(i) != _a.at(i))
         {
@@ -469,4 +480,10 @@ inline void Vector<T>::swap(Vector &v)
     std::swap(sz, v.sz);
     std::swap(cap, v.cap);
     std::swap(data, v.data);
+}
+
+template<typename T>
+inline void Vector<T>::clear()
+{
+    while (sz) { pop_back(); }
 }
